99dd5c6106d63f2c60781172b0a7dacce4511fe3
Hilder64 <aleksei.nimich@gmail.com>
2019-05-02 21:23:21 +0300

    initial commit

---
+++ src/main/java/com/nimich/spring/TestSpring.java
@@ -0,0 +1,15 @@
+package com.nimich.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TestBean bean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(bean.getName());
+
+        context.close();
+    }
+}
